2bcb8002ad7acad71e4d96d585456939950efa08
Milk-Cool <coder.tomsk@gmail.com>
2025-06-22 10:30:44 +0300

    a bit of settings polishing

--- public/main.js
+++ public/main.js
@@ -12,25 +12,33 @@ const fetchStats = async key => {
     });
     if(f.status !== 200) return null;
     return (await f.json()).data.grand_total;
-}
+};
 
 /** @type {HTMLInputElement} */
 const apiKeyInput = document.querySelector("#key");
+/** @type {HTMLButtonElement} */
+const apiKeyButton = document.querySelector("#submitKey");
 const saveAPIKey = async () => {
+    apiKeyButton.disabled = true;
     const newKey = apiKeyInput.value;
     const test = await fetchStats(newKey);
+    apiKeyButton.disabled = false;
     if(test === null)
         return vscode.postMessage({
             command: "alert",
             msg: "Your provided API key is invalid!"
-        });    
+        });
     vscode.postMessage({
         command: "saveKey",
         key: newKey
     });
     apiKey = newKey;
+    return vscode.postMessage({
+        command: "alert",
+        msg: "Updated your API key!"
+    });  
 };
-document.querySelector("#submitKey").addEventListener("click", saveAPIKey);
+apiKeyButton.addEventListener("click", saveAPIKey);
 
 const update = async () => {
     if(!apiKey) {
@@ -48,5 +56,6 @@ const updateCountdown = () => {
     document.querySelector("#countdown").innerText = remaining < 0
         ? "0d 0h 0m 0s"
         : `${Math.floor(remaining / (1000 * 3600 * 24))}d ${Math.floor(remaining / (1000 * 3600)) % 24}h ${Math.floor(remaining / (1000 * 60)) % 60}m ${Math.floor(remaining / 1000) % 60}s`;
-}
+};
+updateCountdown();
 setInterval(updateCountdown, 1000);
